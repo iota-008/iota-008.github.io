d567ba8957c1d0ce63122b99ec7bf8b5c2fa0813
Ankit Raibole <raiboleankit8@gmail.com>
2021-06-14 00:34:20 +0530

    seo friendly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,10 +13,7 @@ export default function App({ Component, pageProps }) {
           content="Portfolio, Programmer, Student, Fullstack Developer"
         />
         <meta name="author" content="Ankit Raibole" />
-        <meta
-          property="og:image"
-          content="../../public/images/Portfolio-Thumbnail.png"
-        />
+        <meta property="og:image" content="/images/Portfolio-Thumbnail.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Ankit Raibole - Portfolio</title>
